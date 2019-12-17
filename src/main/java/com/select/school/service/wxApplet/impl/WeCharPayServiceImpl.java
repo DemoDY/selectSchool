368caa803f52cad7ebb1bf7a600c701ffd955755
dxm <2510103@qq.com>
2019-12-17 14:37:16 +0800

    接收数据接口

--- src/main/java/com/select/school/service/wxApplet/impl/WeCharPayServiceImpl.java
+++ src/main/java/com/select/school/service/wxApplet/impl/WeCharPayServiceImpl.java
@@ -73,7 +73,7 @@ public class WeCharPayServiceImpl implements WeCharPayService {
                     SortedMap<String, Object> parameter = new TreeMap<String, Object>();
                     parameter.put("appid", WeChatAPIParams.WECHAR_PAY_APPID);                      //  app_id
                     parameter.put("timeStamp",timeStamp);
-                    parameter.put("nonce_str", RandomStringUtils.randomAlphanumeric(16));    // 随机字符串 nonce_str 16位
+                    parameter.put("nonceStr", RandomStringUtils.randomAlphanumeric(16));    // 随机字符串 nonce_str 16位
                     parameter.put("package",payResulet.get("prepay_id"));
                     parameter.put("signType","MD5");
                     // 签名 sign(MD5)
